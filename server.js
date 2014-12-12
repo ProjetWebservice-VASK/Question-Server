c2f5a48fe2485bf81fb621e9f5581db7d5dc8018
edouardsouan <souan.edouard0@gmail.com>
2014-12-12 12:43:43 +0100

    [ADD]
    Added swagger dependency ( swagger-node-express)

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var PORT = 9000;
 
 //server nodeJS
 var express = require('express');
-var server = express();
 var mongoose = require('mongoose');
+var swagger = require('swagger-node-express');
+var server = express();
 
 mongoose.connect('mongodb://localhost/questions');
 var db = mongoose.connection;
