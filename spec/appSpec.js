2ccc7619787734a85513c6e25024c9a06d39643b
edouardsouan <souan.edouard0@gmail.com>
2015-03-04 10:44:43 +0100

    Add mocha

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,10 +1,15 @@
 var request = require('supertest');
 var app = require('./../app');
 
-request(app)
-    .get('/')
-    .expect(200)
-    .end(function (error) {
-        if(error) throw error;
-        console.log('Done');
+describe('Request to the root Path', function () {
+
+    it("Should return a 200 status code",function(done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end(function (error) {
+                if(error) throw error;
+                done();
+            });
     });
+});
