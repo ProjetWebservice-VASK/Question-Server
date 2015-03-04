f643e53d365f41cb8b5aab4987f95333684839c2
Vincent Kelleher <k2r79@hotmail.fr>
2015-03-04 16:24:24 +0100

    Configuration File

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    development: "mongodb://localhost/questions-dev",
+    test: "mongodb://admin:admin@ds063870.mongolab.com:63870/test-questions",
+    production: "mongodb://admin:admin@ds063870.mongolab.com:63870/questions"
+  }
+};
