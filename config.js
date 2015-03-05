73b1e8b6ef87ece9720b97f66792d49a550e128a
Vincent Kelleher <k2r79@hotmail.fr>
2015-03-05 10:49:45 +0100

    New database configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   db: {
-    development: "mongodb://localhost/questions-dev",
-    testing: "mongodb://admin:admin@ds063870.mongolab.com:63870/test-questions",
+    development: "mongodb://admin:admin@ds063870.mongolab.com:63870/test-questions",
+    testing: "mongodb://localhost/dev-questions",
     production: "mongodb://admin:admin@ds063870.mongolab.com:63870/questions"
   }
 };
