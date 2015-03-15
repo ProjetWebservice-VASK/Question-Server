102e55c621038b5a7514eb38d760fa508c60010a
Vincent Kelleher <k2r79@hotmail.fr>
2015-03-15 12:30:54 +0100

    Enabled CORS support

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ db.on('error', console.error.bind(console, 'connection error'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+/**
+ * Enable CORS
+ */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/questions', router);
 
 module.exports =app;
