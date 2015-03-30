523e4597e5a1e866058b7726a44775415a54c466
Vincent Kelleher <k2r79@hotmail.fr>
2015-03-30 15:04:49 +0200

    Basic documentation

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ var host = '127.0.0.1';
 var port = (process.env.PORT || 8081);
 var url = 'http://' + host + ':' + port;
 
-var framework = swagger.Framework({ basePath: url });
+var framework = swagger.Framework({basePath: url});
 
 var questionApi = framework.api({
     path: '/questions',
@@ -16,90 +16,156 @@ var questionApi = framework.api({
 /**
  * Question resource
  */
-var questionResource = questionApi.resource({ path: '/questions' });
+var questionResource = questionApi.resource({path: '/questions'});
 
 /**
  * GET all the questions
  */
 questionResource.operation({
-        method: 'GET',
-        summary: 'Retrieve all the questions',
-        notes: 'questions are ordered by descending date',
-        type: 'Question',
-        nickname: 'getAll',
-        parameters: [],
-        responseMessages: [
-            {
-                code: 200,
-                message: 'OK'
-            }
-        ]
-    }
-);
+    method: 'GET',
+    summary: 'Retrieve all the questions',
+    notes: 'questions are ordered by descending date',
+    type: 'Question',
+    nickname: 'getAll',
+    parameters: [],
+    responseMessages: [
+        {
+            code: 200,
+            message: 'OK'
+        }
+    ]
+});
 
 /**
  * POST a question
  */
 questionResource.operation({
-        method: 'POST',
-        summary: 'Create a new question',
-        notes: '',
-        type: 'Question',
-        nickname: 'create',
-        parameters: [
-            {
-                name: 'question',
-                description: 'The sentence of the question',
-                required: true,
-                type: 'string',
-                paramType: 'path'
-            }
-        ],
-        responseMessages: [
-            {
-                code: 201,
-                message: 'OK'
-            }
-        ]
-    }
-);
+    method: 'POST',
+    summary: 'Create a new question',
+    notes: '',
+    type: 'Question',
+    nickname: 'create',
+    parameters: [
+        {
+            name: 'question',
+            description: 'The sentence of the question',
+            required: true,
+            type: 'string',
+            paramType: 'path'
+        }
+    ],
+    responseMessages: [
+        {
+            code: 201,
+            message: 'OK'
+        }
+    ]
+});
 
 /**
  * Single Question resource
  */
-var singleQuestionResource = questionApi.resource({ path: '/questions/{id}' });
+var singleQuestionResource = questionApi.resource({path: '/questions/{id}'});
 
 /**
  * GET a single question
  */
 singleQuestionResource.operation({
-        method: 'GET',
-        summary: 'Retrieve a question',
-        notes: '',
-        type: 'Question',
-        nickname: 'get',
-        parameters: [
-            {
-                name: 'id',
-                description: 'ID of the question to fetch',
-                required: true,
-                type: 'string',
-                paramType: 'path'
-            }
-        ],
-        responseMessages: [
-            {
-                code: 200,
-                message: 'OK'
-            }
-        ]
-    }
-);
+    method: 'GET',
+    summary: 'Retrieve a question',
+    notes: '',
+    type: 'Question',
+    nickname: 'get',
+    parameters: [
+        {
+            name: 'id',
+            description: 'ID of the question to fetch',
+            required: true,
+            type: 'string',
+            paramType: 'path'
+        }
+    ],
+    responseMessages: [
+        {
+            code: 200,
+            message: 'OK'
+        }
+    ]
+});
+
+/**
+ * Question received resource
+ */
+var questionReceivedResource = questionApi.resource({path: '/questions/{id}/received'});
+
+/**
+ * POST the confirmation of the question's reception
+ */
+questionReceivedResource.operation({
+    method: 'GET',
+    summary: 'Confirm the question\'s reception',
+    notes: 'has to be sent once the next pending question is retrieved',
+    type: 'none',
+    nickname: 'confirmReception',
+    parameters: [
+        {
+            name: 'id',
+            description: 'ID of the question',
+            required: true,
+            type: 'string',
+            paramType: 'path'
+        }
+    ],
+    responseMessages: [
+        {
+            code: 204,
+            message: 'No Content'
+        }
+    ]
+});
+
+/**
+ * Question answer resource
+ */
+var questionAnswerResource = questionApi.resource({path: '/questions/{id}/answer'});
+
+/**
+ * POST the question's answer
+ */
+questionAnswerResource.operation({
+    method: 'PUT',
+    summary: 'Answer the question',
+    notes: '',
+    type: 'none',
+    nickname: 'answerQuestion',
+    parameters: [
+        {
+            name: 'id',
+            description: 'ID of the question',
+            required: true,
+            type: 'string',
+            paramType: 'path'
+        },
+        {
+            name: 'answer',
+            description: 'Answer to the question',
+            required: true,
+            type: 'string',
+            paramType: 'form'
+        }
+    ],
+    responseMessages: [
+        {
+            code: 204,
+            message: 'No Content'
+        }
+    ]
+});
 
 /**
  * Next question resource
  */
-var nextQuestionResource = questionApi.resource({ path: '/questions/next' });
+var nextQuestionResource = questionApi.resource({path: '/questions/next'});
 
 /**
  * GET the next pending question
@@ -149,7 +215,7 @@ questionApi.model({
 if (module.parent) {
     module.exports = framework;
 } else {
-    framework.server().listen(port, host, function(err) {
+    framework.server().listen(port, host, function (err) {
         if (err) throw err;
 
         console.log('Server started ' + url + '/');
